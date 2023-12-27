ca1740efba62baa4ee2131e244bb3efb4bceef84
Lemlem-yo <ltsifete@gmail.com>
2023-12-27 20:33:55 +0300

    our final project

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 import 'SecondPage.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key});
@@ -39,7 +42,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF12A19A),
+      backgroundColor: Color(0xFF12A19A),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -114,11 +117,204 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       const SizedBox(height: 20),
-                      // Add other TextFormField widgets here
+                      TextFormField(
+                        controller: _email,
+                        onChanged: (value) {
+                          setState(() {
+                            _email.text = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Email",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Enter Email',
+                          hintStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _phoneNumber,
+                        keyboardType: TextInputType.phone,
+                        onChanged: (value) {
+                          setState(() {
+                            _phoneNumber.text = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter phone number';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Phone Number",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Enter phone number',
+                          hintStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _describRole,
+                        onChanged: (value) {
+                          setState(() {
+                            _describRole.text = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Your Role here';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Describe Your Role:"
+                                "\n(eg. author, podcast hoster,)",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Enter Your Role',
+                          hintStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _sex,
+                        onChanged: (value) {
+                          setState(() {
+                            _sex.text = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Your Role here';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Sex/Gender:"
+                                "\n(eg. Female or Male,)",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Enter Your sex/Gender',
+                          hintStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _age,
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          setState(() {
+                            _age.text = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Your Role here';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text(
+                            "Age",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Enter Your Age',
+                          hintStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF12A19A),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
                       Container(
                         padding: EdgeInsets.all(6.0),
                         decoration: BoxDecoration(
-                          border: Border.all(color: const Color(0xFF12A19A), width: 1),
+                          border: Border.all(color: Color(0xFF12A19A), width: 1),
                           borderRadius: BorderRadius.circular(3),
                         ),
                         child: DropdownButton(
@@ -147,14 +343,18 @@ class _HomePageState extends State<HomePage> {
                       ElevatedButton(
                         onPressed: () {
                           _submitForm();
+                          print("register");
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => SecondPage()),
+                          );
                         },
                         child: const Text(
                           'Notify Me',
                           style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF12A19A),
-                          ),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF12A19A)),
                         ),
                       ),
                     ],
@@ -162,11 +362,63 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
+            const SizedBox(height: 20),
+            ClipRect(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Center(
+                    child: Text(
+                      'Follow us on social media:',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.facebook),
+                        onPressed: () {
+                          _launchURL('https://www.facebook.com/EwenetCommunicationPLC/');
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.email),
+                        onPressed: () {
+                          _launchURL('mailto:ewenetnet@gmail.com');
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.link),
+                        onPressed: () {
+                          //  LinkedIn link here
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+  //
+  void _launchURL(String url) async {
+    try {
+      await launch(url);
+    } catch (e) {
+      print('Error launching URL: $e');
+    }
+  }
+
+
+
 
   Future<void> _submitForm() async {
     String name = _name.text;
@@ -176,9 +428,11 @@ class _HomePageState extends State<HomePage> {
     String sex = _sex.text;
     String age = _age.text;
 
-    String apiUrl = 'http://192.168.42.172:3000/registration/register';
+    // Update the API URL with your actual server URL
+    String apiUrl = 'http://192.168.100.183:3000/registration/register';
 
-    Map<String, String> userData = {
+    // Construct the user data
+    Map<String, dynamic> userData = {
       'name': name,
       'email': email,
       'phoneNumber': phoneNumber,
@@ -187,15 +441,18 @@ class _HomePageState extends State<HomePage> {
       'age': age,
     };
 
+    // Convert the user data to JSON format
     String jsonData = jsonEncode(userData);
 
     try {
+      // Make a POST request to the NestJS API
       var response = await http.post(
         Uri.parse(apiUrl),
         headers: {'Content-Type': 'application/json'},
         body: jsonData,
       );
 
+      // Check if the request was successful (status code 200)
       if (response.statusCode == 200) {
         print('Registration successful');
         _name.clear();
@@ -205,7 +462,7 @@ class _HomePageState extends State<HomePage> {
         _sex.clear();
         _age.clear();
         setState(() {
-          valueChoose = '';
+          valueChoose = ''; // Reset dropdown value
         });
       } else {
         print('Error submitting form. Status code: ${response.statusCode}');
